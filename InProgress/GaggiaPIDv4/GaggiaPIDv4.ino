af19138e952585068d3a82d7440f2b891e2bc3b5
Sam Rausch <samrausch@gmail.com>
2025-06-02 11:51:42 -0400

    disabled wifi by default

--- InProgress/GaggiaPIDv4/GaggiaPIDv4.ino
+++ InProgress/GaggiaPIDv4/GaggiaPIDv4.ino
@@ -1,11 +1,13 @@
+//#define debug
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WebServer.h>
 #include <LEAmDNS.h>
 
 #ifndef STASSID
-#define STASSID "hotcockmachine"
-#define STAPSK "starbucks"
+#define STASSID "wifi_network"
+#define STAPSK "wifi_password"
 #endif
 
 const char* ssid = STASSID;
@@ -18,24 +20,30 @@ WebServer server(80);
 Adafruit_MCP9600 tc;
 
 #include <Bounce2.h>
-//Bounce modeButton = Bounce();
 Bounce2::Button modeButton = Bounce2::Button();
 Bounce2::Button upButton = Bounce2::Button();
 Bounce2::Button downButton = Bounce2::Button();
+//Bounce modeButton = Bounce();
+//Bounce upButton = Bounce();
+//Bounce downButton = Bounce();
+int modePushCounter = 0;
+int modeDebounced = 0;
+int modeLastDebounced = 0;
+bool modePressed = false;
 const unsigned long intervalMs = 5;
 
-#define NOOP __asm__ __volatile__ ("nop\n\t")
+#define NOOP __asm__ __volatile__("nop\n\t")
 
 #include <Adafruit_SSD1306.h>
 #include <gfxfont.h>
 #include <Adafruit_GFX.h>
 
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 #define OLED_RESET 4
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-#include <Wire.h> 
+#include <Wire.h>
 
 const int indent1 = 88;
 const int indent2 = 72;
@@ -43,15 +51,15 @@ const int bottom = 56;
 const int line1 = 0;
 const int line2 = 16;
 const int line3 = 32;
-const int upPin = 8; // Pi Pico pin
-const int modePin = 7; // Pi Pico pin
-const int downPin = 6; // Pi Pico pin
-const int relayPin = 9; // Pi Pico pin
-int operatingMode = 1; // 1=brew 2=steam
-int configureMode = 1; // 1=operate 2=configure
-int PIDparam = 1; // 1=Kp 2=Ki 3=Kd
-int steamTemp = 140; // default steam temp
-int brewTemp = 85; // default brew temp
+const int upPin = 27;    // Pi Pico pin
+const int modePin = 26;  // Pi Pico pin
+const int downPin = 28;  // Pi Pico pin
+const int relayPin = 9;  // Pi Pico pin
+int operatingMode = 1;   // 1=brew 2=steam
+int configureMode = 1;   // 1=operate 2=configure
+int PIDparam = 1;        // 1=Kp 2=Ki 3=Kd
+int steamTemp = 140;     // default steam temp
+int brewTemp = 85;       // default brew temp
 
 
 #include <PID_v1.h>
@@ -69,27 +77,46 @@ unsigned long calcStartTime;
 
 
 void setup() {
-  Wire.setSDA(0);  Wire.setSCL(1);  Wire.begin();
+  Serial.begin(9600);
+#ifdef debug
+  Serial.println("Debugging enabled...");
+#endif
+  Wire.setSDA(0);
+  Wire.setSCL(1);
+  Wire.begin();
   pinMode(relayPin, OUTPUT);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  display.clearDisplay();  display.setCursor(0,0);  display.setTextSize(2); display.setTextColor(WHITE);
+  display.clearDisplay();
+  display.setCursor(0, 0);
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
   display.println("Gaggia PID");
   display.println("Pico");
   display.println("v1.0");
   delay(2000);
-  if (! tc.begin(tc_address)) {
-      display.clearDisplay();
-      display.println("TC_mcp ERROR");
-      display.display();
-      while (1);
-    }
+  if (!tc.begin(tc_address)) {
+    display.clearDisplay();
+    display.println("TC_mcp ERROR");
+    display.display();
+    while (1)
+      ;
+  }
   tc.setADCresolution(MCP9600_ADCRESOLUTION_18);
   tc.setThermocoupleType(MCP9600_TYPE_K);
   tc.setFilterCoefficient(3);
   tc.enable(true);
-  pinMode(modePin, INPUT_PULLUP);  modeButton.attach(modePin);  modeButton.interval(intervalMs);  modeButton.setPressedState(LOW);
-  pinMode(upPin, INPUT_PULLUP);  upButton.attach(modePin);  upButton.interval(intervalMs);  upButton.setPressedState(LOW);
-  pinMode(downPin, INPUT_PULLUP);  downButton.attach(modePin);  downButton.interval(intervalMs);  downButton.setPressedState(LOW);
+  pinMode(modePin, INPUT_PULLUP);
+  modeButton.attach(modePin);
+  modeButton.interval(intervalMs);
+  modeButton.setPressedState(LOW);
+  pinMode(upPin, INPUT_PULLUP);
+  upButton.attach(upPin);
+  upButton.interval(intervalMs);
+  upButton.setPressedState(LOW);
+  pinMode(downPin, INPUT_PULLUP);
+  downButton.attach(downPin);
+  downButton.interval(intervalMs);
+  downButton.setPressedState(LOW);
   windowStartTime = millis();
   calcStartTime = millis();
   targetTemp = brewTemp;
@@ -100,19 +127,19 @@ void setup() {
   Serial.println("");
 
   // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+//  while (WiFi.status() != WL_CONNECTED) {
+//    delay(500);
+//    Serial.print(".");
+//  }
   Serial.println("");
   Serial.print("Connected to ");
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("gaggiapid")) {
-    Serial.println("MDNS responder started");
-  }
+//  if (MDNS.begin("gaggiapid")) {
+//    Serial.println("MDNS responder started");
+//  }
   server.on("/", handleRoot);
   server.onNotFound(handleNotFound);
   server.on("/inline", []() {
@@ -123,6 +150,13 @@ void setup() {
 }
 
 void loop() {
+#ifdef debug
+  Serial.print(configureMode);
+  Serial.print(",");
+  Serial.print(operatingMode);
+  Serial.print(",");
+  Serial.println(PIDparam);
+#endif
   server.handleClient();
   MDNS.update();
   checkButtons();
